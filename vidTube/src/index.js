10a55d899beb4284a955635cc15485b036d2f7ef
gopi-trip <gopitrip.gt123@gmail.com>
2025-03-18 16:03:25 +0530

    Add CORS support, dotenv configuration, and MongoDB connection setup

--- vidTube/src/index.js
+++ vidTube/src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello from backend!");
+import {app} from './app.js';
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+const PORT = process.emitWarning.PORT ||7001
+
+dotenv.config({
+    path: "./.env"
+});
+
+connectDB()
+.then(() => {
+    app.listen(PORT,() => {
+        console.log(`Server is running at port ${PORT}....`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB Connection Error: ",err);
+    
+})

c69f7dd2c37925997752164b33d51055d2d9311c
gopi-trip <gopitrip.gt123@gmail.com>
2025-03-18 18:56:12 +0530

    Add utility classes for API error handling and response management

---
+++ vidTube/src/utils/asyncHandler.js
@@ -0,0 +1,8 @@
+
+const asycnHandler = (requestHandler) => {
+    return (req,res,next) => {
+        Promise.resolve(reqHandler(req,res,next)).catch((err) => next(err))
+    }
+}
+
+export {asycnHandler}

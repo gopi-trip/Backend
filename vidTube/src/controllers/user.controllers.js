6244092a9272e899c08c51d8a6884e707a1d1dcb
gopi-trip <gopitrip.gt123@gmail.com>
2025-04-05 15:56:39 +0530

    Add loginUser function and enhance token generation in user controller

--- vidTube/src/controllers/user.controllers.js
+++ vidTube/src/controllers/user.controllers.js
@@ -4,6 +4,23 @@ import {User} from '../models/user.models.js'
 import {uploadOnCloudinary,deleteFromCloudinary} from '../utils/cloudinary.js'
 import { apiResponse } from "../utils/apiResponse.js";
 
+const generateAccessAndRefreshTokens = async (userId) => {
+   try {
+     const user = await User.findById(userId)
+     if(!user){
+         throw new apiError(404,"User could not be found")
+     }
+     const accessToken = user.generateAccessToken()
+     const refreshToken = user.generateRefreshToken()
+ 
+     user.refreshToken = refreshToken
+     await user.save({validateBeforeSave:false})
+     return {accessToken,refreshToken}
+   } catch (error) {
+        throw new apiError(404,"Something went wrong while generating access and refresh tokens",error)
+   }
+}
+
 const registerUser = asyncHandler(
     async (req,res) => {
         const {fullname,email,username,password} = req.body
@@ -96,6 +113,56 @@ const registerUser = asyncHandler(
     }
 )
 
+const loginUser = asyncHandler(
+    async(req,res) => {
+        //1.Get data from body
+        const {username,email,password} = req.body
+        //2.Run Checks if you want
+        if(!email){
+            throw new apiError(400,"Email is required")
+        }
+
+        const user = await User.findOne({
+            $or: [{username},{email}]
+        })
+
+        if(!user){
+            throw new apiError(404,"User not found")
+        }
+
+        //3.Validate Password
+        const isPasswordValid = await user.isPasswordCorrect(password)
+        
+        if(!isPasswordValid){
+            throw new apiError(401,"Invalid Credentials")
+        }
+
+        const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+        const loggedInUser = await User.findById(user._id)
+            .select("-password -refreshToken")
+
+        const options = {
+            httpOnly: true, //This makes the cookie non-modifiable from the client side, only we can modify this
+            secure: process.env.NODE_ENV === "production"
+        }
+
+        //4. Send this data
+        return res
+            .status(200)
+            .cookie("accessToken",accessToken,options)
+            .cookie("refreshToken",refreshToken,options)
+            .json(new apiResponse(
+                200,
+                { user: loggedInUser,accessToken,refreshToken },
+                "User logged in successfully"
+            ))
+
+    }
+)
+
+
 export { 
-    registerUser
+    registerUser,
+    loginUser
  }

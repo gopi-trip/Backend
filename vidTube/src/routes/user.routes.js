c17d71eed5112880f25dc183b18a09905e8d6b5d
gopi-trip <gopitrip.gt123@gmail.com>
2025-04-18 17:41:36 +0530

    Refactor user.routes.js to include additional routes for login, refresh token, change password, current user retrieval, account updates, and improve organization of imports

--- vidTube/src/routes/user.routes.js
+++ vidTube/src/routes/user.routes.js
@@ -1,11 +1,25 @@
 import { Router } from 'express'
 
-import { registerUser,logoutUser } from '../controllers/user.controllers.js';
+import { 
+    registerUser,
+    logoutUser, 
+    loginUser,
+     refreshAccessToken, 
+     changeCurrentPassword,
+     getCurrentUser,
+     getUserChannelProfile,
+     updateAccountDetails,
+     updateUserAvatar,
+     updateUserCoverImage,
+     getWatchHistory
+    } from '../controllers/user.controllers.js';
 import { upload } from '../middlewares/multer.middlewares.js'
 import { verifyJWT } from '../middlewares/auth.middlewares.js';
 
 
 const router = Router();
+
+//Unsecured Routes
 
 router.route('/register').post(
     upload.fields([
@@ -21,7 +35,21 @@ router.route('/register').post(
     registerUser
 )
 
+router.route('/login').post(loginUser)
+router.route('/refresh-token').post(refreshAccessToken)
+
 //Secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
+router.route('/change-password').post(verifyJWT,changeCurrentPassword)
+router.route('/current-user').get(verifyJWT,getCurrentUser)
+router.route('/c/:username').get(verifyJWT,getUserChannelProfile)
+//For updation we use patch
+router.route('/update-account').patch(verifyJWT,updateAccountDetails)
 
+//Image file
+router.route('/avatar').patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route('/cover-image').patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+//Watch History
+router.route('/history').get(verifyJWT,getWatchHistory)
 export default router
